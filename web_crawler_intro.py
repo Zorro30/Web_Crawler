e9bed006cd21092509f1feb88117331f0d072a32
Gaurang Patel <gbp301195@gmail.com>
2018-07-05 15:21:56 +0530

    Delete web_crawler_intro.py

--- web_crawler_intro.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#function to create a directory
-def create_dir(directory):
-    if not os.path.exists(directory):
-        print ("Creating the {} directory".format(directory))
-        os.makedirs(directory)
-
-create_dir('Hello')
